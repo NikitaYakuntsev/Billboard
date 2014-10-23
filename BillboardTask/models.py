a3fa67afcb25c000ce6b0fe0665c4cc47904d362
NikitaYakuntsev <nikitayakuntsev@yandex.ru>
2014-10-23 10:22:01 +0400

    Models added

--- BillboardTask/models.py
+++ BillboardTask/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    id_user = models.IntegerField(primary_key=True)
+    login = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    description = models.TextField(max_length=1000)
+
+class Category(models.Model):
+    id_category = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=30)
+    adverts = models.ManyToManyField(Advert, through=CategoryAndAdvert)
+
+
+class Advert(models.Model):
+    id_advert = models.IntegerField(primary_key=True)
+    id_user = models.ForeignKey(User)
+    title = models.CharField(max_length=50)
+    text = models.TextField(max_length=1000)
+    price = models.IntegerField()
+    date = models.DateField()
+    id_report = models.ForeignKey(Report)
+
+class CategoryAndAdvert(models.Model):
+    id_advert = models.ForeignKey(Advert)
+    id_category = models.ForeignKey(Category)
+
+class Report(models.Model):
+    id_report = models.IntegerField(primary_key=True)
+    text = models.TextField(max_length=300)
