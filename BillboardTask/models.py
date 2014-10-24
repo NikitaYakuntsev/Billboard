67555dd03ad0181c5ebad4ade03dedfce4b3631e
NikitaYakuntsev <nikitayakuntsev@yandex.ru>
2014-10-24 18:59:00 +0400

    added print methods for models for admin panel

--- BillboardTask/models.py
+++ BillboardTask/models.py
@@ -7,12 +7,16 @@ class User(models.Model):
     login = models.CharField(max_length=30)
     password = models.CharField(max_length=30)
     description = models.TextField(max_length=1000)
+    def __unicode__(self):
+        return str(self.id) +". " + str(self.login)
 
 
 class Report(models.Model):
     #id_report = models.IntegerField(primary_key=True)
     text = models.TextField(max_length=300)
     id_advert = models.ForeignKey('Advert')
+    def __unicode__(self):
+        return str(id)
 
 
 class Advert(models.Model):
@@ -23,15 +27,21 @@ class Advert(models.Model):
     price = models.IntegerField()
     date = models.DateField()
     categories = models.ManyToManyField('Category', through='CategoryAndAdvert')
+    def __unicode__(self):
+        return str(self.id) + ". " + str(self.title)
 
 class Category(models.Model):
     #id_category = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=30, unique=True)
     #adverts = models.ManyToManyField(Advert, through=CategoryAndAdvert)
+    def __unicode__(self):
+        return str(self.id) + ". " + str(self.name)
 
 
 class CategoryAndAdvert(models.Model):
     id_advert = models.ForeignKey(Advert)
     id_category = models.ForeignKey(Category)
+    def __unicode__(self):
+        return str(self.id_advert_id) + " & " + str(self.id_category_id)
 
 
